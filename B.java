c6aa46f8e1eb5bfe667832419fb36c1da597ca97
KRISHNA DIPAYAN BHUNIA <kri.subsc+github@gmail.com>
2023-08-19 20:53:55 +0530

    multithread correction

--- B.java
+++ B.java
@@ -1,10 +1,8 @@
 public class B extends Thread {
     public void run() {
         try {
-            int var_job2 = 100;
             System.out.println("this is job 2 of class B");
             Thread.sleep(3000);
-            var_job2 = 200;
             System.out.println("this is job 2 of class B and modified variable");
         } catch (Exception ex) {
             System.out.println("there is an exception" + ex);
