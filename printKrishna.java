17eada3b1ef23d497645a1f715ea418366e8cba0
KRISHNA DIPAYAN BHUNIA <23618035+krishnabhunia@users.noreply.github.com>
2023-08-23 14:23:21 +0530

    commit 15

--- printKrishna.java
+++ printKrishna.java
@@ -2,6 +2,6 @@ public class printKrishna {
     public static void main(String[] args) {
         System.out.println("printKrishna.main and thats great");
         System.out.println("okay all mer");
-        System.out.println("branch 14");
+        System.out.println("branch 15 with new");
     }
 }
