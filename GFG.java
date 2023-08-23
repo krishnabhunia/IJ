1046a2ab9e2e36ee8d19e6392ca3c7efee2ef850
KRISHNA DIPAYAN BHUNIA <kri.subsc+github@gmail.com>
2023-08-23 21:56:53 +0530

    from boc

--- GFG.java
+++ GFG.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class GFG {
+final class Base {
+
+    void Display() {
+        System.out.print("Method for Base class.");
+    }
+}
+
+class Extended extends Base {
+
+    void Display() {
+        System.out.print("Method of Extended class.");
+    }
+}
+
+class GFG {
+
+    public static void main(String[] arg) {
+        Extended d = new Extended();
+        d.Display();
+    }
 }
